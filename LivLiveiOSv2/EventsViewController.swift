6e280402619aac22fe73d0d54a3603d377b8a31a
Michael Droz <michael.droz@tcc-mdroz.local>
2014-11-28 08:33:29 -0800

    Removed duplicate ViewController in EventDetailController.xib with help from Oliver. Baseline working version of UITableView controller.

--- LivLiveiOSv2/EventsViewController.swift
+++ LivLiveiOSv2/EventsViewController.swift
@@ -25,8 +25,7 @@ class EventsViewController: UITableViewController {
     
     
     override init(style: UITableViewStyle) {
-        println("I am inside override init")
-//        TODO: Add Style .Plain 
+        //        TODO: Add Style .Plain
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
         session = NSURLSession(configuration: config, delegate: nil, delegateQueue: nil)
         
@@ -48,16 +47,12 @@ class EventsViewController: UITableViewController {
     }
     
     override func viewDidLoad() {
-       // tableView.delegate = self
-       // tableView.dataSource = self
-        println("I am inside viewDidLoad")
-
+      
         //Load the NIB file
         let nib = UINib(nibName: "EventCell", bundle: nil)
         
         //REgister the NIB, which contains the cell
         tableView.registerNib(nib, forCellReuseIdentifier: "EventCell")
-        println("I am about to call fectchFeed")
         fetchFeed()
        // super.viewDidLoad()
         
@@ -78,9 +73,7 @@ class EventsViewController: UITableViewController {
         println("About to get a new or receyvled cell")
         //Get a new or recycled cell 
         let cell = tableView.dequeueReusableCellWithIdentifier("EventCell", forIndexPath: indexPath) as EventCell
-        println("about to go into let event = events...after deque")
-         let event = events[indexPath.row]
-        println("Trying to print cell.eventTitle")
+        let event = events[indexPath.row]
         //Configure the cell with the item
         if let title: AnyObject = event["title"] {
             if let titleString = title as? String {
@@ -117,7 +110,6 @@ class EventsViewController: UITableViewController {
     
     func fetchFeed() {
         
-        println("Quarantine Begins Here")
         let requestString = "http://private-8618e-michaeldroz.apiary-mock.com/events"
         if let url = NSURL(string: requestString) {
             let req = NSURLRequest(URL: url)
@@ -132,7 +124,6 @@ class EventsViewController: UITableViewController {
                             if let cs = eventArray as? [[NSObject:AnyObject]] {
                                 self.events = cs
                                 println("\(self.events)")
-                                println("printing here below self.events")
                                 NSOperationQueue.mainQueue().addOperationWithBlock() {
                                     println("about to execute reloadSections")
                                     println("about to preint event.soucn \(self.events.count)")
